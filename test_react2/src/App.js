9b09e4b4970e757aff450d3a5b285e5a4d4c1f99
Gretel Ortiz <ada_hack348@hackademy.mx>
2022-04-21 17:42:00 -0500

    Agregamos pagina de Personal Information

--- test_react2/src/App.js
+++ test_react2/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import LoginForm from "./components/LoginForm/LoginForm";
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 import PresentationForm from "./components/PerfilSolicitante/PresentationForm";
+import PersonalInformation from "./components/PerfilSolicitante/PersonalInformation/PersonalInformation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<RegistrationForm/>}/>
           <Route path="/login" element={<LoginForm/>}/>
           <Route path="/presentation" element={<PresentationForm/>}/>
+          <Route path="/PersonalInformation" element={<PersonalInformation/>}/>
         </Routes>
       </BrowserRouter>
     </div>
